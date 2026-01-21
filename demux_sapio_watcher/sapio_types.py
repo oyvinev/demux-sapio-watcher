52b5528c6e34d742619616b73c25791257726251
Øyvind Evju <oyvind.evju@medisin.uio.no>
2026-01-21 13:06:59 +0100

    Add QC metrics to SequencingFile

--- demux_sapio_watcher/sapio_types.py
+++ demux_sapio_watcher/sapio_types.py
@@ -2,9 +2,9 @@ import re
 from pathlib import Path
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, computed_field
 
-from demux_sapio_watcher.bclconvert.models import CombinedSampleData
+from demux_sapio_watcher.bclconvert.models import CombinedSampleData, QualityMetrics
 
 UUID_RE = re.compile(
     r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
@@ -55,6 +55,84 @@ class SequencingFile(SapioRecord):
         ..., alias="AllFilesAvailable", description="Whether all files are available"
     )
 
+    readsPf: int | None = Field(
+        None,
+        description="# Reads from demux stats",
+    )
+
+    pfClustersPercentPerLane: float | None = Field(
+        None,
+        description="% Reads from demux stats",
+    )
+    perfectIndexReadPercent: float | None = Field(
+        None,
+        description="% Perfect Index Reads from demux stats",
+    )
+
+    oneMismatchIndexReadPercent: float | None = Field(
+        None,
+        description="% One Mismatch Index Reads from demux stats",
+    )
+
+    yieldPfGb: float | None = Field(
+        None,
+        description="Yield (in Gb) - sum of R1 and R2 yield",
+    )
+
+    basesQ30Percent: float | None = Field(
+        None,
+        description="% Bases with quality score >= Q30",
+    )
+
+    averageQScore: float | None = Field(
+        None,
+        description="Quality score sum / yield",
+    )
+
+    @computed_field
+    @property
+    def dataReadPasses(self) -> None | int:
+        "Return the number of data reads (1 or 2) based on available FASTQ paths."
+        if self.fastq_path_R1 and self.fastq_path_R2:
+            return 2
+        if self.fastq_path_R1:
+            return 1
+        return None
+
+    @computed_field
+    @property
+    def oraCompressionEnabled(self) -> None | bool:
+        "Return True if ORA compression is enabled (deduced from R1 file extension)."
+        if not self.fastq_path_R1:
+            return None
+
+        return ".ora" in self.fastq_path_R1.name
+
+    @computed_field
+    @property
+    def sampleSheetPosition(self) -> int | None:
+        "Return the sample sheet position (S##) extracted from R1 file name."
+        if not self.fastq_path_R1:
+            return None
+        position_re = r".*?_S(\d+)_.*?"
+        m = re.match(position_re, self.fastq_path_R1.name)
+        if m:
+            return int(m.group(1))
+        return None
+
+    @computed_field
+    @property
+    def onboardAnalysisType(self) -> str | None:
+        "Return the onboard analysis type based on the R1 file path."
+        if not self.fastq_path_R1:
+            return None
+
+        if "/DragenGermline/" in str(self.fastq_path_R1):
+            return "DragenGermline"
+        if "/BCLConvert" in str(self.fastq_path_R1):
+            return "BCLConvert"
+        return None
+
     @classmethod
     def from_bclconvert(cls, combined_data: CombinedSampleData) -> SequencingFile:
         sample_guid = UUID_RE.search(combined_data.fastq.sample_id)
@@ -62,6 +140,19 @@ class SequencingFile(SapioRecord):
             raise ValueError(
                 f"Unable to extract sample GUID from {combined_data.fastq.sample_id}"
             )
+
+        qc1 = combined_data.quality_metrics_read1
+        if combined_data.quality_metrics_read2 is None:
+            # Create a dummy qc2 to simplify code below - but ensure that yield is zero
+            qc2 = QualityMetrics(**qc1.model_dump(by_alias=True))
+            qc2.read_number = 2
+            qc2.yield_ = 0
+            qc2.yield_q30 = 0
+            qc2.quality_score_sum = 0
+
+        else:
+            qc2 = combined_data.quality_metrics_read2
+
         return cls(
             RecordId=None,
             SampleGuid=sample_guid.group(),
@@ -71,4 +162,14 @@ class SequencingFile(SapioRecord):
             FASTQ_path_I2=None,
             SampleName=combined_data.fastq.sample_id,
             AllFilesAvailable=True,  # If we have a CombinedSampleData object, then this is True
+            # QC metrics from demux stats:
+            readsPf=combined_data.demux_stats.num_reads,
+            perfectIndexReadPercent=combined_data.demux_stats.percent_perfect_index_reads,
+            oneMismatchIndexReadPercent=combined_data.demux_stats.percent_one_mismatch_index_reads,
+            pfClustersPercentPerLane=combined_data.demux_stats.percent_reads,
+            # Combined qc metrics from qc1 and qc2
+            yieldPfGb=(qc1.yield_ + qc2.yield_) / 1_000_000_000,
+            basesQ30Percent=(qc1.yield_q30 + qc2.yield_q30) / (qc1.yield_ + qc2.yield_),
+            averageQScore=(qc1.quality_score_sum + qc2.quality_score_sum)
+            / (qc1.yield_ + qc2.yield_),
         )
--- tests/test_model_conversion.py
+++ tests/test_model_conversion.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from demux_sapio_watcher.bclconvert.parse_folder import parse_bclconvert_folder
+
+import hypothesis as ht
+
 from demux_sapio_watcher.bclconvert.find_folders import find_bclconvert_folders
+from demux_sapio_watcher.bclconvert.parse_folder import parse_bclconvert_folder
 from demux_sapio_watcher.sapio_types import SequencingFile
 from tests.conftest import runfolder
 from tests.data_generation import PairedReadSampleTestData, build_samples
-
-import hypothesis as ht
 
 
 @ht.given(build_samples())
@@ -33,3 +34,66 @@ def test_payload(samples: list[PairedReadSampleTestData]):
             assert "dataTypeName" in payload
             assert "recordId" in payload
             assert "fields" in payload
+
+
+@ht.given(build_samples())
+def test_qc_metrics(samples: list[PairedReadSampleTestData]):
+    with runfolder(samples) as rf:
+        bclconvert_folders = find_bclconvert_folders([Path(rf.root)])
+        assert len(bclconvert_folders) == 1
+        bclconvert_folder = bclconvert_folders.pop()
+        data = list(parse_bclconvert_folder(bclconvert_folder))
+        assert len(data), "No data parsed from BCLConvert folder"
+
+        for item in data:
+            # Check first for paired ends
+            converted = SequencingFile.from_bclconvert(item)
+            assert item.quality_metrics_read2 is not None, (
+                "Expected paired-end data for this test"
+            )
+            assert converted.dataReadPasses == 2
+            assert converted.readsPf == item.demux_stats.num_reads
+            assert converted.pfClustersPercentPerLane == item.demux_stats.percent_reads
+            assert (
+                converted.perfectIndexReadPercent
+                == item.demux_stats.percent_perfect_index_reads
+            )
+            assert (
+                converted.oneMismatchIndexReadPercent
+                == item.demux_stats.percent_one_mismatch_index_reads
+            )
+            assert (
+                converted.yieldPfGb
+                == (
+                    item.quality_metrics_read1.yield_
+                    + item.quality_metrics_read2.yield_
+                )
+                / 1e9
+            )
+
+            assert converted.basesQ30Percent == (
+                item.quality_metrics_read1.yield_q30
+                + item.quality_metrics_read2.yield_q30
+            ) / (item.quality_metrics_read1.yield_ + item.quality_metrics_read2.yield_)
+
+            assert converted.averageQScore == (
+                item.quality_metrics_read1.quality_score_sum
+                + item.quality_metrics_read2.quality_score_sum
+            ) / (item.quality_metrics_read1.yield_ + item.quality_metrics_read2.yield_)
+
+            # Check for single-end data by removing read 2
+            item.fastq.read2_file = None
+            item.quality_metrics_read2 = None
+            converted_se = SequencingFile.from_bclconvert(item)
+            assert item.quality_metrics_read2 is None, (
+                "Expected single-end data for this test"
+            )
+            assert converted_se.dataReadPasses == 1
+            assert converted_se.yieldPfGb == item.quality_metrics_read1.yield_ / 1e9
+            assert converted_se.basesQ30Percent == (
+                item.quality_metrics_read1.yield_q30 / item.quality_metrics_read1.yield_
+            )
+            assert converted_se.averageQScore == (
+                item.quality_metrics_read1.quality_score_sum
+                / item.quality_metrics_read1.yield_
+            )
